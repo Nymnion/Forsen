9cc2c6566b43f709419cd3ea0e36e2589a543539
Nerixyz <nerixdev@outlook.de>
2023-04-04 21:23:51 +0200

    fix: format everything and fix missing tags

--- js/easterEgg.js
+++ js/easterEgg.js
@@ -1,20 +1,18 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const pepePainsButton = document.getElementsByClassName("line-button")[0];
-    console.log(pepePainsButton);
-    pepePainsButton.addEventListener("click", countTowardsEasterEgg);
+document.addEventListener('DOMContentLoaded', function () {
+  const pepePainsButton = document.getElementsByClassName('line-button')[0];
+  console.log(pepePainsButton);
+  pepePainsButton.addEventListener('click', countTowardsEasterEgg);
 
-    let counter = 0;
+  let counter = 0;
 
-    function countTowardsEasterEgg() {
-        counter++;
-        if(counter === 25){
-            executeEasterEgg();
-        }
+  function countTowardsEasterEgg() {
+    counter++;
+    if (counter === 25) {
+      executeEasterEgg();
     }
+  }
 
-    function executeEasterEgg() {
-        pepePainsButton.classList.add("animateEasterEgg");
-    }
- 
- 
-  });
+  function executeEasterEgg() {
+    pepePainsButton.classList.add('animateEasterEgg');
+  }
+});
