b3d9dbb79d8a032e77335e2541742117e82d73d7
jp1995 <Johanpoldaru@gmail.com>
2023-04-05 03:27:56 +0300

    hopefully these embeds work on the live site Prayge

--- js/clips.js
+++ js/clips.js
@@ -1,24 +1,48 @@
 $.get('https://www.reddit.com/r/LivestreamFail/search/.json?sort=top&q=flair%3Atwitch%3A%2BForsen&restrict_sr=on&t=month', function(response) {
-  var clips = response.data.children;
-  var topClips = clips.filter(function(clip) {
-    return clip.data.url.includes('https://clips.twitch.tv/') && !clip.data.url.includes('/embed');
-  }).slice(0, 8);
-  displayClips(topClips);
+    let clips = response.data.children;
+    let topClips = clips.filter(function(clip) {
+        return clip.data.url.includes('https://clips.twitch.tv/') && !clip.data.url.includes('/embed');
+    }).slice(0, 8);
+    displayClips(topClips);
 });
 
 function displayClips(clips) {
-  var $clipsDiv = $('#clips');
-  clips.forEach(function(clip) {
-    var $clipDiv = $('<div>').addClass('clip');
-    var $titleLink = $('<a>').attr('href', clip.data.url).attr('target', '_blank').text(clip.data.title);
+    let $clipsDiv = $('#clips');
+    clips.forEach(function(clip) {
+        let $clipDiv = $('<div>').addClass('clip');
+        let $titleLink = $('<a>').attr('href', 'https://www.reddit.com' + clip.data.permalink).attr('target', '_blank').text(clip.data.title);
 
-    var thumbnailUrl = clip.data.thumbnail;
-    if (thumbnailUrl === 'nsfw') {
-        thumbnailUrl = '../images/placeholder.png';
-    }
-    var $thumbnailLink = $('<a>').attr('href', clip.data.url).attr('target', '_blank').append($('<img>').attr('src', thumbnailUrl).attr('alt', clip.data.title));
-    $clipDiv.append($thumbnailLink).append($titleLink);
-    $clipsDiv.append($clipDiv);
-  });
+        let thumbnailUrl = clip.data.thumbnail;
+        // this will also cover deleted clips
+        if (thumbnailUrl === 'nsfw') {
+            thumbnailUrl = '../images/placeholder.png';
+        }
+        let $thumbnailLink = $('<a>').addClass('clip-thumbnail').attr('href', '#').append($('<img>').attr('src', thumbnailUrl).attr('alt', clip.data.title)).data('clip', clip);
+        $clipDiv.append($thumbnailLink).append($titleLink);
+        $clipsDiv.append($clipDiv);
+    });
+
+    $('.clip-thumbnail').click(function(event) {
+        event.preventDefault();
+        let $thumbnailLink = $(this);
+        let clip = $thumbnailLink.data('clip');
+        let slug = clip.data.url.split('/').pop();
+        let iframeWidth = $(window).width() * 0.6;
+        let iframeHeight = Math.floor(iframeWidth * 9 / 16);
+        let $overlay = $('<div>').addClass('overlay');
+        let $iframe = $('<iframe>').attr('src', 'https://clips.twitch.tv/embed?clip=' + slug + '&parent=forsen.site').attr('height', iframeHeight + 'px').attr('width', iframeWidth + 'px').attr('allowfullscreen', true);
+        let $closeButton = $('<div>').click(function(event) {
+            event.preventDefault();
+            $overlay.remove();
+        });
+
+        $overlay.click(function(event) {
+            if (event.target === this || event.target === $closeButton[0] || event.target === $iframe[0]) {
+                $overlay.remove();
+            }
+        });
+
+        $overlay.append($iframe).append($closeButton);
+        $('body').append($overlay);
+    });
 }
-
